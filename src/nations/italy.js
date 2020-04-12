181a263926ac9175af07ec3e96c32023aba055ae
MassimoVlacancich <mv607@york.ac.uk>
2020-04-12 13:31:26 +0100

    added google analytics

--- src/nations/italy.js
+++ src/nations/italy.js
@@ -202,7 +202,7 @@ export function ItalyFormHook(props) {
                             </div>
                         }
                         {isMobile &&
-                            <p className="suggestion">(apri e click sull anno in alto a sinistra per selezionare l'anno)</p>
+                            <p className="suggestion">(Android: apri e click sull anno in alto a sinistra per selezionare l'anno)</p>
                         }
                     </div>
                     <div className="col-md-6 mb-3">
@@ -286,7 +286,7 @@ export function ItalyFormHook(props) {
                             </div>
                         }
                         {isMobile &&
-                            <p className="suggestion">(apri e click sull anno in alto a sinistra per selezionare l'anno)</p>
+                            <p className="suggestion">(Android: apri e click sull anno in alto a sinistra per selezionare l'anno)</p>
                         }
                     </div>
                 </div>
